de2213aaebf0fdcfab2b980f256627c173941345
Santi871 <santivegega@gmail.com>
2019-05-20 21:27:06 -0300

    implemented logger

---
+++ src/logger.py
@@ -0,0 +1,26 @@
+import logging
+from sys import stdout
+
+
+with open("log.txt", "w+") as f:
+    f.write("----waypoints.ini----\n\n")
+
+    with open("waypoints.ini", "r") as f2:
+        f.writelines(f2.readlines())
+
+    f.write("\n\n----settings.ini----\n\n")
+    with open("settings.ini", "r") as f2:
+        f.writelines(f2.readlines())
+
+    f.write("\n\n--------------------\n\n")
+
+
+logger = logging.getLogger('root')
+formatter = logging.Formatter('%(levelname)s - %(message)s')
+s_handler = logging.StreamHandler(stdout)
+s_handler.setFormatter(formatter)
+f_handler = logging.FileHandler('log.txt')
+f_handler.setFormatter(formatter)
+logger.addHandler(f_handler)
+logger.addHandler(s_handler)
+logger.setLevel(logging.INFO)
