fbc4a55ecd92e05be05bc2f4d0f33043edb969ca
Santi871 <santivegega@gmail.com>
2019-05-23 02:25:25 -0300

    remove parsing of waypoints config file, replace with GUI

--- main.py
+++ main.py
@@ -1,40 +1,14 @@
-from time import sleep
 from configparser import ConfigParser
-import sys
 from src.logger import logger
 from src.wp_editor import WaypointEditor
-from src.gui import get_gui, run_gui
+from src.gui import GUI
 
 
 def main(config):
-    preferences = config['PREFERENCES']
+    editor = WaypointEditor(config, logger)
 
-    try:
-        editor = WaypointEditor(sys.argv[1], config, logger)
-    except IndexError:
-        editor = WaypointEditor('waypoints.ini', config, logger)
-
-    active_wps, active_msns = editor.build_msns_and_wps()
-
-    window = get_gui(active_wps, active_msns)
-    run_gui(window)
-
-    if active_wps or active_msns:
-        for i in reversed(range(preferences.getint('Grace_Period', 5))):
-            logger.info(f"Entering data in {i+1}...")
-            sleep(1)
-    else:
-        logger.info("No active preplanned missions or waypoints defined")
-
-    if active_wps:
-        logger.info(f"Entering {len(active_wps)} waypoints")
-        # editor.enter_waypoints(active_wps)
-
-    sleep(1)
-
-    if active_msns:
-        logger.info(f"Entering {len(active_msns)} PP missions")
-        # editor.enter_missions(active_msns)
+    gui = GUI(editor)
+    gui.run()
 
 
 if __name__ == "__main__":
