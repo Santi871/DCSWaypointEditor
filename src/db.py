f590e9019ba529c5151b4fbe16faf74922cb26f0
Santi871 <santivegega@gmail.com>
2019-05-23 02:21:35 -0300

    initial commit

---
+++ src/db.py
@@ -0,0 +1,51 @@
+from models import ProfileModel, MissionModel, WaypointModel, db
+from objects import MSN, Wp
+from LatLon23 import LatLon, Latitude, Longitude
+
+
+class DatabaseInterface:
+    def __init__(self, db_name, logger):
+        self.logger = logger
+        db.init(db_name)
+        db.connect()
+        db.create_tables([ProfileModel, MissionModel, WaypointModel])
+        logger.debug("Connected to database")
+
+    def get_profile(self, profilename):
+        profile = ProfileModel.get(ProfileModel.name == profilename)
+        msns = [MSN(LatLon(Latitude(mission.latitude), Longitude(mission.longitude)),
+                    elevation=mission.elevation, name=mission.name) for mission in profile.missions]
+
+        wps = [Wp(LatLon(Latitude(waypoint.latitude), Longitude(waypoint.longitude)),
+                  elevation=waypoint.elevation, name=waypoint.name) for waypoint in profile.waypoints]
+
+        self.logger.debug(f"Fetched {profilename} from DB, with {len(msns)} missions and {len(wps)} waypoints")
+        return msns, wps
+
+    def save_profile(self, name, msns, wps):
+        missionmodel_instances = list()
+        waypointmodel_instances = list()
+
+        profile = ProfileModel.create(name=name)
+
+        self.logger.debug(f"Attempting to save profile {name}: {msns} // {wps}")
+        for mission in msns:
+            missionmodel_instance = MissionModel.create(name=mission.name,
+                                                        latitude=mission.position.lat.decimal_degree,
+                                                        longitude=mission.position.lon.decimal_degree,
+                                                        elevation=mission.elevation,
+                                                        number=mission.number,
+                                                        profile=profile)
+            missionmodel_instances.append(missionmodel_instance)
+
+        for waypoint in wps:
+            waypointmodel_instance = WaypointModel.create(name=waypoint.name,
+                                                          latitude=waypoint.position.lat.decimal_degree,
+                                                          longitude=waypoint.position.lon.decimal_degree,
+                                                          elevation=waypoint.elevation,
+                                                          profile=profile)
+            waypointmodel_instances.append(waypointmodel_instance)
+
+    @staticmethod
+    def close():
+        db.close()
---
+++ src/models.py
@@ -0,0 +1,33 @@
+from peewee import *
+
+db = SqliteDatabase(None, pragmas={'foreign_keys': 1})
+
+
+class ProfileModel(Model):
+    name = CharField(unique=True)
+
+    class Meta:
+        database = db
+
+
+class MissionModel(Model):
+    number = IntegerField()
+    name = CharField(null=True, default="")
+    latitude = FloatField()
+    longitude = FloatField()
+    elevation = FloatField()
+    profile = ForeignKeyField(ProfileModel, backref='missions')
+
+    class Meta:
+        database = db
+
+
+class WaypointModel(Model):
+    name = CharField(null=True, default="")
+    latitude = FloatField()
+    longitude = FloatField()
+    elevation = FloatField()
+    profile = ForeignKeyField(ProfileModel, backref='waypoints')
+
+    class Meta:
+        database = db
