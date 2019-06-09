45b3d12721d4bf27c373d6c5df8a80899e3cbcb8
Santi871 <santivegega@gmail.com>
2019-06-09 19:25:00 -0300

    fix slash missing from DCS path when using browse button

--- src/first_setup.py
+++ src/first_setup.py
@@ -32,19 +32,23 @@ def first_time_setup():
     while True:
         event, values = gui.Read()
 
+        dcs_path = values.get("dcs_path")
+        if dcs_path is not None and not dcs_path.endswith("\\") and not dcs_path.endswith("/"):
+            dcs_path = dcs_path + "\\"
+
         if event is None:
             return False
         elif event == "accept_button":
             break
         elif event == "install_button":
             try:
-                install_dcs_bios(values["dcs_path"])
+                install_dcs_bios(dcs_path)
                 gui.Element("install_button").Update(disabled=True)
                 gui.Element("accept_button").Update(disabled=False)
                 gui.Element("dcs_bios").Update(value="Installed")
             except (FileExistsError, FileNotFoundError):
                 gui.Element("dcs_bios").Update(value="Failed to install")
-                setup_logger.error("DCS-BIOS failed to installed", exc_info=True)
+                setup_logger.error("DCS-BIOS failed to install", exc_info=True)
         elif event == "dcs_path":
             dcs_bios_detected = detect_dcs_bios(values["dcs_path"])
             if dcs_bios_detected:
@@ -60,7 +64,7 @@ def first_time_setup():
     config.add_section("PREFERENCES")
     config.set("PREFERENCES", "grace_period", "5")
     config.set("PREFERENCES", "tesseract_path", values.get("tesseract_path") or default_tesseract_path)
-    config.set("PREFERENCES", "dcs_path", values.get("dcs_path") or default_dcs_path)
+    config.set("PREFERENCES", "dcs_path", dcs_path or default_dcs_path)
     config.set("PREFERENCES", "db_name", "profiles.db")
     config.set("PREFERENCES", "capture_key", values.get("capture_key") or "ctrl+t")
     config.set("PREFERENCES", "log_raw_tesseract_output", "false")
